7ff2e7fc5811aae309f4ac40a5b3d234328651e3
Teradad41 <sitiang120@gmail.com>
2025-11-11 23:31:34 +0900

    予定の表示・追加まで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,106 @@
+use chrono::NaiveDateTime;
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter},
+};
+
+const SCHEDULE_FILE: &str = "schedule.json";
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct Schedule {
+    id: u64,
+    subject: String,
+    start: NaiveDateTime,
+    end: NaiveDateTime,
+}
+
+impl Schedule {
+    fn new(id: u64, subject: String, start: NaiveDateTime, end: NaiveDateTime) -> Self {
+        Self {
+            id,
+            subject,
+            start,
+            end,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct Calendar {
+    schedules: Vec<Schedule>,
+}
+
+#[derive(Parser)]
+struct App {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// 予定の一覧表示
+    List,
+    /// 予定の追加
+    Add {
+        /// タイトル
+        subject: String,
+        /// 開始日時
+        start: NaiveDateTime,
+        /// 終了日時
+        end: NaiveDateTime,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = App::parse();
+
+    match options.command {
+        Commands::List => show_list(),
+        Commands::Add {
+            subject,
+            start,
+            end,
+        } => add_schedule(subject, start, end),
+    }
+}
+
+fn show_list() {
+    let calendar: Calendar = {
+        let file = File::open(SCHEDULE_FILE).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    };
+
+    // 予定の表示
+    println!("ID\tSTART\tEND\tSUBJECT");
+    for schedule in calendar.schedules {
+        println!(
+            "{}\t{}\t{}\t{}",
+            schedule.id, schedule.start, schedule.end, schedule.subject
+        );
+    }
+}
+
+fn add_schedule(subject: String, start: NaiveDateTime, end: NaiveDateTime) {
+    let mut calendar: Calendar = {
+        let file = File::open(SCHEDULE_FILE).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    };
+
+    // 予定の作成
+    let id = calendar.schedules.len() as u64;
+    let new_schedule = Schedule::new(id, subject, start, end);
+    calendar.schedules.push(new_schedule);
+
+    // 予定の保存
+    {
+        let file = File::create(SCHEDULE_FILE).unwrap();
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &calendar).unwrap()
+    }
+
+    println!("予定を追加しました！");
 }
